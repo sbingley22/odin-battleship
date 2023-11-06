99f318e7e90da90987a971142c59b39008b37733
sbingley22 <sbingley22@gmail.com>
2023-11-06 16:40:37 +0000

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-battleship
+Simple battleship game made for the odin course.
+Live Link:
+https://sbingley22.github.io/odin-battleship
---
+++ src/Ship.js
@@ -0,0 +1,7 @@
+export default class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+    this.hits = 0;
+  }
+}
