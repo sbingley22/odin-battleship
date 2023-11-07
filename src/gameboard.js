c38e614fb89442593a4289eedbd42fe502037458
sbingley22 <sbingley22@gmail.com>
2023-11-07 10:42:58 +0000

    working on sinking ships

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,9 +1,63 @@
+import Ship from "./ship";
+
 export default class Gameboard {
   constructor() {
     this.board = new Array(8 * 8).fill("");
+    this.ships = {};
   }
 
-  canPlaceShip(name, startSquare, length, horizontal) {
+  shipsRemaining() {
+    let shipsLeft = 0;
+
+    for (const key in this.ships) {
+      const ship = this.ships[key];
+      if (ship.sunk == false) shipsLeft += 1;
+    }
+
+    return shipsLeft;
+  }
+
+  receiveAttack(attack) {
+    const square = this.board[attack];
+    if (square === "hit" || square === "miss") return null;
+
+    if (square === "") {
+      this.board[attack] = "miss";
+      return "miss";
+    } else {
+      const shipName = this.board[attack];
+      this.ships[shipName].hit(attack);
+      this.board[attack] = "hit";
+      return shipName;
+    }
+  }
+
+  placeShip(name, startSquare, length, horizontal) {
+    const squares = this.canPlaceShip(startSquare, length, horizontal);
+    if (squares == false) return false;
+
+    if (this.isShipBlocking(squares)) return false;
+
+    squares.forEach((square) => {
+      this.board[square] = name;
+    });
+
+    this.ships[name] = new Ship(name, squares);
+
+    return true;
+  }
+
+  isShipBlocking(squares) {
+    return squares.some((square) => {
+      return (
+        this.board[square] !== "" &&
+        this.board[square] !== "hit" &&
+        this.board[square] !== "miss"
+      );
+    });
+  }
+
+  canPlaceShip(startSquare, length, horizontal) {
     if (startSquare < 0 || startSquare > this.board.length) return false;
 
     if (horizontal) {
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -4,16 +4,82 @@ const gameboard = new Gameboard();
 
 describe("test ship placement", () => {
   it("tests horizontal placement as false", () => {
-    expect(gameboard.canPlaceShip("boat", 6, 3, true)).toBe(false);
+    expect(gameboard.canPlaceShip(6, 3, true)).toBe(false);
   });
   it("tests horizontal placement as valid", () => {
-    expect(gameboard.canPlaceShip("boat", 5, 3, true)).toEqual([5, 6, 7]);
+    expect(gameboard.canPlaceShip(5, 3, true)).toEqual([5, 6, 7]);
   });
 
   it("tests vertical placement as false", () => {
-    expect(gameboard.canPlaceShip("boat", 55, 3, false)).toBe(false);
+    expect(gameboard.canPlaceShip(55, 3, false)).toBe(false);
   });
   it("tests vertical placement as valid", () => {
-    expect(gameboard.canPlaceShip("boat", 47, 3, false)).toEqual([47, 55, 63]);
+    expect(gameboard.canPlaceShip(47, 3, false)).toEqual([47, 55, 63]);
+  });
+
+  it("places ship ontop of another", () => {
+    gameboard.board[3] = "ship";
+    expect(gameboard.isShipBlocking([1, 2, 3])).toBe(true);
+  });
+  it("places ship ontop of another", () => {
+    gameboard.board[3] = "ship";
+    expect(gameboard.placeShip("cruise", 1, 3, true)).toBe(false);
+  });
+  it("places ship ontop of another", () => {
+    gameboard.board[3] = "ship";
+    gameboard.placeShip("cruise", 1, 3, true);
+    expect(gameboard.board[3]).toEqual("ship");
+  });
+
+  it("places ship near another", () => {
+    gameboard.board[(3, 11)] = "ship";
+    expect(gameboard.placeShip("cruise", 1, 3, false)).toBe(true);
+  });
+  it("places ship near another", () => {
+    gameboard.board[(3, 11)] = "ship";
+    gameboard.placeShip("cruise", 1, 3, false);
+    expect(gameboard.board[1] == "cruise");
+  });
+});
+
+describe("ship attacks", () => {
+  it("recieves attack that misses", () => {
+    expect(gameboard.receiveAttack(7)).toBe("miss");
+  });
+
+  it("recieves attack that hits", () => {
+    gameboard.placeShip("battle", 4, 4, true);
+    expect(gameboard.receiveAttack(7)).toBe("battle");
+  });
+});
+
+describe("sunken ships", () => {
+  beforeEach(() => {
+    gameboard.placeShip("battle", 4, 3, true);
+    gameboard.placeShip("boat", 1, 2, true);
+  });
+
+  it("returns 2 ships left", () => {
+    expect(gameboard.shipsRemaining()).toBe(2);
+  });
+
+  it("returns 2 ships left", () => {
+    gameboard.receiveAttack(1);
+    expect(gameboard.shipsRemaining()).toBe(2);
+  });
+
+  it("returns 1 ship left", () => {
+    gameboard.receiveAttack(1);
+    gameboard.receiveAttack(2);
+    expect(gameboard.shipsRemaining()).toBe(1);
+  });
+
+  it("returns 1 ship left", () => {
+    gameboard.receiveAttack(1);
+    gameboard.receiveAttack(2);
+    gameboard.receiveAttack(3);
+    gameboard.receiveAttack(4);
+    gameboard.receiveAttack(5);
+    expect(gameboard.shipsRemaining()).toBe(0);
   });
 });
